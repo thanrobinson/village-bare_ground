a8ba9f0c8cb4ed40da3a33a6b35619f8f652b9aa
Nathaniel Robinson <nathanielpaulrobinson@gmail.com>
2024-11-15 07:47:57 -0500

    total ha to proportion

--- R/analysis.r
+++ R/analysis.r
@@ -2,14 +2,22 @@
 glad <- read.csv("data/glad_mod44_bgr_areas.csv")
 rap <- read.csv("data/rap_mod44_bgr_areas.csv")
 
+head(glad)
+glad$glad_bgr_prop <- glad$glad_bgr_area / glad$glad_area
+glad$mod_bgr_prop <- glad$mod_bgr_area / glad$mod_area
+
+
+
 # GLAD Bare Ground vs Modis Bare Ground @Village Level
-with(glad, plot(glad_bgr_area, mod_bgr_area,
+with(glad, plot(glad_bgr_prop, mod_bgr_prop,
   main = "Village Bare Ground Area (2010)",
   xlab = "GLAD Bare Ground Area (ha)",
-  ylab = "MOD44B Bare Ground Area (ha)"
+  ylab = "MOD44B Bare Ground Area (ha)",
+  xlim = c(0, 0.5),
+  ylim = c(0, 0.5)
 ))
-abline(1, 1)
-with(glad, cor(glad_bgr_area, mod_bgr_area))
+abline(0, 1)
+with(glad, cor(glad_bgr_prop, mod_bgr_prop))
 
 # RAP Bareg Ground vs Modis Bare Ground - random sample
 rap$mod_percent_dif <- (rap$mod_bgr_post - rap$mod_bgr_pre) /
@@ -26,7 +34,7 @@ with(rap, plot(rap_percent_dif, mod_percent_dif,
   xlab = "RAP Change in Bare Ground (%)",
   ylab = "MOD44B Change in Bare Ground (%)",
 ))
-abline(1, 1, col = "grey")
+abline(0, 1, col = "grey")
 with(rap, cor(rap_dif, mod_dif))
 
 rap$sign1 <- ifelse(rap$mod_di > 0, "Positive", "Negative")
